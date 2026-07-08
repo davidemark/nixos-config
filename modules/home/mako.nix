163dd277144f913dee6bc11dd41123362df8b3f7
davidemark <davidemark.1993@gmail.com>
2026-07-08 22:50:07 +0200

    fix mako

--- modules/home/mako.nix
+++ modules/home/mako.nix
@@ -5,7 +5,7 @@
     settings = {
       background-color = "#303446dd";
       text-color = "#c6d0f5";
-      border-color = "#8caaee dd";
+      border-color = "#8caaeedd";
       border-radius = 8;
       border-size = 2;
       font = "JetBrainsMono Nerd Font 12";
@@ -20,7 +20,7 @@
       border-color=#a6d189dd
 
       [urgency=normal]
-      border-color=#8caaee dd
+      border-color=#8caaeedd
 
       [urgency=high]
       border-color=#e78284dd
